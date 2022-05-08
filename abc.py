98a11103d220f30010dc44a6d39132265c37e6dc
deebigaM <104816669+deebigaM@users.noreply.github.com>
2022-05-08 12:39:41 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from math import pi 
+r= float (input("enter radius of circle 1"))
+a = pi * r *r
+print ("area of the circle is : ",a)
+ 
+ 
